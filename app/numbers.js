7245d427723ec1e3d9122a4dab590aa8d04aa868
sathibabu <psathibabu@gmail.com>
2017-06-02 09:53:18 +0530

    numbers partially done

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+  	let str = num.toString(2);
+    let index = str.length-bit;
+    return parseInt(str.charAt(index));
   },
 
   base10: function(str) {
-
+        return parseInt(str,2);
   },
 
   convertToBinary: function(num) {
 
+  		return ('00000000'+num.toString(2)).substr(-8);
   },
 
   multiply: function(a, b) {
-
+  		return (a*b).toFixed(1);
   }
 };
